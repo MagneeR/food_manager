60ce313eeefcca14eaf9d71fa832262483dcd265
Your NameMagneeR <magneer47@gmail.com>
2026-04-20 19:59:52 +0200

    Create frontend folder

--- remote_build.sh
+++ remote_build.sh
@@ -1,12 +1,11 @@
 cd food_manager
 python -m venv .venv
-source \.venv\Scripts\Activate
+source .venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 rm -rf public
 export REFLEX_LOGLEVEL=default
 reflex init
-# Add my backend api url 
 #REFLEX_API_URL=https://api.moure.dev reflex export --frontend-only
 unzip frontend.zip -d public
 rm -f frontend.zip
